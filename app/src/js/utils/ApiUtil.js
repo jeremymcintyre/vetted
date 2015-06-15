8caa0c9514dfeb6b6a7f79d870aebd6bc3a1acb1
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-06-15 15:20:23 -0700

    Increase number of seed activities to make demo more realistic for typical use case

--- app/src/js/utils/ApiUtil.js
+++ app/src/js/utils/ApiUtil.js
@@ -18,9 +18,17 @@ var ApiUtils = {
   loadActivities () {
     ServerActionCreators.loadedActivities([
         {name: "SO", notes: "Get the wings"},
-        {name: "Bear Brewing", notes: "ESB Nitro is amazing"},
+        {name: "ThirstyBear Brewing Co", notes: "ESB Nitro is amazing"},
         {name: "Sextant", notes: "Cappuccino, mocha or cold brew"},
-        {name: "Laguna", notes: "Awesome hookah / open late night life"}
+        {name: "Laguna", notes: "Awesome hookah / open late night life"},
+        {name: "Sightglass", notes: "Picturesque, cappuccino is amazing"},
+        {name: "Golden Gate Park", notes: "Rent bikes, go to the beach"},
+        {name: "Garaje", notes: "Fish/shrimp tacos or pulled pork"},
+        {name: "Alembic", notes: "Last word - can be loud"},
+        {name: "Osaka Sushi", notes: "Sunday happy hour at 4pm"},
+        {name: "Green Chile", notes: "Best breakfast burritos"},
+        {name: "Cafe Chaat", notes: "Go-to Indian food"},
+        {name: "The Chieftain", notes: "Irish breakfast and a Magners"}
       ]);
   }
 
