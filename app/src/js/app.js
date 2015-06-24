e14d706313b96362368d19cc38623370e72ed13b
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-06-24 16:04:48 -0700

    Lay out new Route structure based on intended UI nesting

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -3,18 +3,34 @@ var Router = require('react-router');
 var { DefaultRoute, Route } = Router;
 
 var App = require('./components/App');
-var FriendsList = require('./components/FriendsList');
-var ActivityList = require('./components/ActivityList');
-var Map = require('./components/Map');
-var About = require('./components/About');
 
 var routes = (
-    <Route handler={ App }>
-      <DefaultRoute name="Activities" handler={ ActivityList } />
-      <Route name="Friends" path="/friends" handler={ FriendsList } />
-      <Route name="Map" path="/map" handler={ Map } />
-      <Route name="About" path="/about" handler={ About } />
+    <Route component={ AppBar }>
+      <Route component={ HomeTabBar }>
+        <Route component={ PickFriend }/>
+        <Route component={ ActivitiesForFriend }/>
+        <Route component={ MapForFriend }/>
+        <Route component={ About }/>
+      </Route>
+      <Route component={ ChooseFriendsTabBar }>
+        <Route component={ AddFriend }
+        <Route component={ ChooseFriends }/>
+      </Route>
+      <Route component={ AddActivityTabBar }>
+        <Route component={ AddActivity }/>
+      </Route>
+      <Route component={ AddLocationTabBar }>
+        <Route component={ AddLocation }/>
+      </Route>
     </Route>
+
+
+    // <Route handler={ App }>
+    //   <DefaultRoute name="Activities" handler={ ActivityList } />
+    //   <Route name="Friends" path="/friends" handler={ FriendsList } />
+    //   <Route name="Map" path="/map" handler={ Map } />
+    //   <Route name="About" path="/about" handler={ About } />
+    // </Route>
 );
 
 Router.run(routes, function(Handler) {
