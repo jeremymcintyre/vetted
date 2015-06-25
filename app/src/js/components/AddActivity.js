28b180a14c3ffefe9f1b3cc5ce6fd0463651af9a
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-06-25 01:43:30 -0700

    Add initial form and styling to AddActivity route, including dynamically colored count of remaining characters

--- app/src/js/components/AddActivity.js
+++ app/src/js/components/AddActivity.js
@@ -2,11 +2,71 @@ var React = require('react');
 var InfoBar = require('./InfoBar');
 
 var AddActivity = React.createClass({
+
+  getInitialState () {
+    return {
+      charsRemaining: "",
+      countClass: ""
+    }
+  },
+
+  countLength (event) {
+    var inputId = event.target.id,
+        inputLength = event.target.value.length,
+        maxLength,
+        charsRemaining;
+
+    if (inputId === "activity-name-input") {
+      maxLength = 40;
+    }
+    else if (inputId === "activity-description-input") {
+      maxLength = 120;
+    }
+
+    charsRemaining = maxLength - inputLength;
+
+    this.setState({
+      charsRemaining: charsRemaining,
+      countClass: this.charCountClass(charsRemaining)
+    });
+  },
+
+  charCountClass (charsRemaining) {
+    if (charsRemaining < 10) {
+      return "low";
+    }
+    else if (charsRemaining < 20) {
+      return "getting-lower";
+    }
+    else if (charsRemaining < 40) {
+      return "getting-low";
+    }
+    else {
+      return "";
+    }
+  },
+
   render () {
     return (
       <div>
         <InfoBar infoText="Describe activity" />
-        <p>Hayyy</p>
+        <form id="activity-form">
+          <input id="activity-name-input"
+                 type="text"
+                 placeholder="Name this thing"
+                 onFocus={ this.countLength }
+                 onChange={ this.countLength } />
+          <textarea id="activity-description-input"
+                 type="text"
+                 placeholder="What's cool about it?"
+                 onFocus={ this.countLength }
+                 onChange={ this.countLength }
+                 selectionStart={120} />
+          <div id="char-count" className={ this.state.countClass }>
+            { this.state.charsRemaining }
+          </div>
+          <input type="submit" name="Next" />
+        </form>
       </div>
     )
   }
